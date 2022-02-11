d7d9ef276b0a20cf246a6d1bb9bf13f1b9197b94
vrinda-mahajan <vrindamahajan96@gmail.com>
2022-02-12 02:25:43 +0530

    Added navigation and modal component

--- src/component.js
+++ src/component.js
@@ -3,3 +3,22 @@ const alertCloseBtn = document.querySelector(".alert-close-icon")
 alertCloseBtn.addEventListener("click",()=>{
     alertCloseBtn.parentElement.classList.add("hide")
 })
+
+// Modal
+
+const openModal = document.querySelector('.btn-open-modal')
+const modal = document.querySelector('.show-modal')
+const closeModal = document.querySelector('#modal-close-btn')
+
+openModal.addEventListener('click',()=>{
+    modal.style.display = 'flex';
+})
+
+closeModal.addEventListener('click',()=>{
+    modal.style.display='none';
+})
+
+window.addEventListener('click',(event)=>{
+    if (event.target === modal){
+    modal.style.display='none'}
+})

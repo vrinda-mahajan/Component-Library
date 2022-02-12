70b1a3e4808cf7c88a04856f7d11c1e3e7736052
vrinda-mahajan <vrindamahajan96@gmail.com>
2022-02-12 21:20:07 +0530

    Added rating and toast component

--- src/component.js
+++ src/component.js
@@ -22,3 +22,17 @@ window.addEventListener('click',(event)=>{
     if (event.target === modal){
     modal.style.display='none'}
 })
+
+// Toast 
+
+const openToastBtn = document.querySelector('.open-toast-btn')
+const toast = document.querySelector('.show-toast')
+const closeToastBtn = document.querySelector('.toast-close-btn')
+
+openToastBtn.addEventListener('click',()=>{
+    toast.style.display='flex';
+    setTimeout(() => {toast.style.display="none"},5000)
+})
+
+closeToastBtn.addEventListener("click",()=>{toast.style.display="none"})
+
